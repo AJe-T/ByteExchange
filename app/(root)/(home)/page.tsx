b14d81ed611f9d8de309cba023d1b95eb9bd77d1
AJe-T <tejateju205101@gmail.com>
2024-06-08 16:11:41 +0530

    Theme functionality added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
   return (
     <>
-      <div className="flex-center min-h-screen w-full">
-        Home
-         <UserButton afterSignOutUrl="/" />
-      </div>
-      ;
+      <div className="flex-center min-h-screen w-full">Home</div>
     </>
   );
 };
